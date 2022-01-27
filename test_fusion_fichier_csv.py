61ee47dd3b38984cfe84e61bfd4354dde08d2d78
Baptiste Antony <92865441+iiiiyama@users.noreply.github.com>
2022-01-27 09:31:13 +0100

    Add files via upload

--- test_fusion_fichier_csv.py
+++ test_fusion_fichier_csv.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
+for i in range (1,6):
+    lecture = pd.read_csv(f"netflix-{i}.csv", header=None)
+    test = lecture.drop(lecture.columns[11], axis=1)
+    test.to_csv(f"output{i}.csv")
+
 df = pd.concat(
-    map(pd.read_csv, ['netflix-1.csv', 'netflix-2.csv','netflix-3.csv','netflix-4.csv','netflix-5.csv']), ignore_index=True)
+    map(pd.read_csv, ['output1.csv', 'output2.csv','output3.csv','output4.csv','output5.csv']), ignore_index=True)
 print(df)
-df.to_csv("test.csv")
+df.to_csv("test.csv", index=False)
