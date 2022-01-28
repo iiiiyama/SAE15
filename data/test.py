8a9a4f4e57e73e37de1c00b75c43bc08073585c5
dragbee <92865220+dragbee@users.noreply.github.com>
2022-01-28 10:30:11 +0100

    Add files via upload

---
+++ data/test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+for i in range(1, 6):
+    lecture = pd.read_csv(f"netflix-{i}.csv", header=None)
+    test = lecture.drop(lecture.columns[11], axis=1)
+    test.to_csv(f"output{i}.csv")
+
+df = pd.concat(
+    map(pd.read_csv, ['output1.csv', 'output2.csv', 'output3.csv', 'output4.csv', 'output5.csv']), ignore_index=True)
+print(df)
+df.to_csv("test.csv", index=False)
